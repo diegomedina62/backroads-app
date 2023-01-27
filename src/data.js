21c8bc1709e73af27fd4847e15356fbfa7df8961
diegomedina62 <diegoleonmedina62@gmail.com>
2023-01-27 16:52:06 -0300

    code refactor in tours component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import img1 from './images/tour-1.jpeg';
+import img2 from './images/tour-2.jpeg';
+import img3 from './images/tour-3.jpeg';
+import img4 from './images/tour-4.jpeg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -45,5 +50,56 @@ export const servicesData = [
     title: 'amazing comfort',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Asperiores, officia.`,
+  },
+];
+
+export const tourData = [
+  {
+    id: 1,
+    img: img1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Cumque vitae tempore voluptatum maxime reprehenderit eum quod
+    exercitationem fugit, qui corporis.`,
+    location: 'China',
+    period: '6 Days',
+    price: 'from $2100',
+  },
+  {
+    id: 2,
+    img: img2,
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Cumque vitae tempore voluptatum maxime reprehenderit eum quod
+    exercitationem fugit, qui corporis.`,
+    location: 'indonesia',
+    period: '11 days',
+    price: 'from $1400',
+  },
+  {
+    id: 3,
+    img: img3,
+    date: 'september 15th, 2020',
+    title: 'explore hong kong',
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Cumque vitae tempore voluptatum maxime reprehenderit eum quod
+    exercitationem fugit, qui corporis.`,
+    location: 'hong kong',
+    period: '8 days',
+    price: 'from $5000',
+  },
+  {
+    id: 4,
+    img: img4,
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Cumque vitae tempore voluptatum maxime reprehenderit eum quod
+    exercitationem fugit, qui corporis.`,
+    location: 'kenya',
+    period: '20 days',
+    price: 'from $3300',
   },
 ];
