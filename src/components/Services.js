e48bed2a63f537cbbe35713212d27f45f8612f2d
diegomedina62 <diegoleonmedina62@gmail.com>
2023-01-27 13:29:01 -0300

    change services to use servicesdata.map

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,5 @@
 import Titles from './Titles';
+import { servicesData } from '../data';
 
 const Services = () => {
   return (
@@ -6,44 +7,19 @@ const Services = () => {
       <Titles title="Our" subtitle="Services" />
 
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {servicesData.map((x) => {
+          return (
+            <article key={x.id} className="service">
+              <span className="service-icon">
+                <i className={x.icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{x.title}</h4>
+                <p className="service-text">{x.text}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
